b228ebe88459420c054f72b210beb7f07895adca
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-10-08 17:18:02 -0300

    now with functional tests environment as well

--- Makefile
+++ Makefile
@@ -9,7 +9,12 @@ dev:
 	@if [ "$(PHANTOMJS_SET)" ]; then echo "phantomjs... \033[32mOK\033[0m"; else echo "phantomjs... \033[31mNOT ok\033[0m"; fi
 	@if [ "$(CASPERJS_SET)" ]; then echo "casperjs... \033[32mOK\033[0m"; else echo "casperjs... \033[31mNOT ok\033[0m"; fi
 
-test:
-	@casperjs test tests/*
+tests-unit:
+	casperjs test tests/unit/*
+
+tests-functional:
+	casperjs test tests/functional/* --fixtures=`pwd`/tests/fixture/
+
+tests-all: tests-unit tests-functional
 
 .PHONY: dev test
---
+++ tests/functional/sanity_test.js
@@ -0,0 +1,16 @@
+(function () {
+    var fixtures_dir = casper.cli.get("fixtures");
+    casper.test.begin("sanity test for functional environment with fixture", 2, function (test) {
+        casper.start("file://" + fixtures_dir + "sanity_check01.html", function () {
+            var self = this, tooltip_id;
+            test.assertTitle("Index");
+            tooltip_id = self.evaluate(function () {
+                return document.querySelectorAll("body > a:nth-child(3)")[0].id;
+            });
+            test.assertEquals(tooltip_id, "link2");
+        });
+        casper.run(function () {
+            test.done();
+        });
+    });
+}());
