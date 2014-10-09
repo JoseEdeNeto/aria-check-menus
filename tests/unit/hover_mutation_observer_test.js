b22ae503c95cea1b5b1d927a7e3c952aea80c120
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-10-09 17:58:20 -0300

    WIP: until observers are ok in PhantomJS

---
+++ tests/unit/hover_mutation_observer_test.js
@@ -0,0 +1,33 @@
+(function () {
+    var fixture_url = casper.cli.get("fixtures");
+    casper.options.clientScripts = ["lib/OverMutationRecorder.js"];
+    casper.test.begin("OverMutationRecorder should record CSS change mutations in target", 1, function (test) {
+        casper.start(fixture_url + "mouseover_mutation_observer01.html", function () {
+            var self = this,
+                result = "";
+            self.mouseEvent("mouseover", "#link2");
+            result = self.evaluate(function () {
+                return window.OverMutationRecorder.popLastEvent();
+            });
+            test.assertEquals(result, "Useful message 2");
+        });
+        casper.run(function () {
+            test.done();
+        });
+    });
+
+    casper.test.begin("OverMutationRecorder should record CSS change mutations in multiple targets", 1, function (test) {
+        casper.start(fixture_url + "mouseover_mutation_observer02.html", function () {
+            var self = this,
+                result = "";
+            self.mouseEvent("mouseover", "#link2");
+            result = self.evaluate(function () {
+                return OverMutationRecorder.popLastEvent();
+            });
+            test.assertEquals(result, "Useful message33");
+        });
+        casper.run(function () {
+            test.done();
+        });
+    });
+}());
