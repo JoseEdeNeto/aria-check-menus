415a1c00671fe665a7adb9587d389e54c1befc6b
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-10-14 13:45:06 -0300

    now registering DOM HTML changes in the mutation as well

--- tests/unit/hover_mutation_observer_test.js
+++ tests/unit/hover_mutation_observer_test.js
@@ -3,6 +3,7 @@
     casper.options.clientScripts = [
         "lib/mutationrecorder/OverMutationRecorder.js",
         "lib/mutationrecorder/ClassNameVerifier.js",
+        "lib/mutationrecorder/InnerHTMLVerifier.js",
         "lib/mutationrecorder/CSSStyleVerifier.js"
     ];
     casper.test.begin("OverMutationRecorder should record CSS change mutations in target", 1, function (test) {
@@ -47,12 +48,32 @@
                 result = "";
             self.evaluate(function () {
                 window.recorder = OverMutationRecorder([ClassNameVerifier, CSSStyleVerifier]);
-            })
+            });
             self.mouseEvent("mouseover", "#link2");
             result = self.evaluate(function () {
                 return recorder.popLastEvent().target.textContent.trim();
             });
             test.assertEquals(result, "Useful message for className changes");
+        });
+        casper.run(function () {
+            test.done();
+        });
+    });
+
+    casper.test.begin("OverMutationRecorder should record innerHTML mutations in targets", 2, function (test) {
+        casper.start(fixture_url + "mouseover_mutation_observer04.html", function () {
+            var self = this,
+                result = "";
+            self.evaluate(function () {
+                window.recorder = OverMutationRecorder([InnerHTMLVerifier]);
+            });
+            self.mouseEvent("mouseover", "#link2");
+            result = self.evaluate(function () {
+                var target = recorder.popLastEvent().target;
+                return [target.tagName, target.innerHTML];
+            });
+            test.assertEquals(result[0], "DIV");
+            test.assertEquals(result[1], "<span>abobrinha</span>");
         });
         casper.run(function () {
             test.done();
