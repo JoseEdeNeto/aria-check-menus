009a9e4da7b58371e478050c46288bd6991a23f6
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-11-26 16:13:49 -0200

    moving architecture for selenium webdriver

--- main.js
+++
@@ -1,6 +0,0 @@
-var app = require("App"),
-    casper = require("casper"),
-    params;
-
-params = app.init({casper: casper, wait: 500});
-app.captureWidgets(params.cli.get(0), "captured_widgets/");
