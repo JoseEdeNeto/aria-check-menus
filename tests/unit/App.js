d84dfb1a24479ab452eced3e86d7465b282a85f8
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-12-03 14:46:46 -0200

    now catching visible elements as well (WIP: needs refactoring)

--- tests/unit/App.js
+++ tests/unit/App.js
@@ -64,4 +64,53 @@ describe("App", function () {
             });
         });
     });
+
+    describe("#get_visible", function () {
+        it("should look for elements using driver", function (done) {
+            var driver_mock = {},
+                app = App(driver_mock),
+                expected_elements = [],
+                result_promise;
+            driver_mock.findElements = function () {
+                arguments.length.should.be.equal(1);
+                arguments[0].should.be.eql({css: "body *"});
+                return Promise(expected_elements);
+            };
+            result_promise = app.get_visibles();
+            result_promise.then(function (result) {
+                result.should.have.length(0);
+                result.should.be.equal(expected_elements);
+                done();
+            });
+        });
+
+        it("should find visible elements in the list", function (done) {
+            var driver_mock = {}, webdriver = {},
+                app = App(driver_mock, webdriver),
+                expected_elements = [],
+                result_promise;
+            driver_mock.findElements = function () {
+                return Promise(expected_elements);
+            };
+            webdriver.promise = {};
+            webdriver.promise.all = function () {
+                arguments.should.have.lengthOf(1);
+                arguments[0].should.have.lengthOf(2);
+                arguments[0][0].should.have.property("id", "p1");
+                arguments[0][1].should.have.property("id", "p2");
+                return Promise([true, false]);
+            };
+            expected_elements[0] = {
+                id: "element1", isDisplayed: function () { var p = Promise(true); p.id = "p1"; return p;} };
+            expected_elements[1] = {
+                id: "element2", isDisplayed: function () { var p = Promise(false); p.id = "p2"; return p;} };
+            result_promise = app.get_visibles();
+            result_promise.then(function (result) {
+                result.should.have.length(1);
+                result.should.containEql(expected_elements[0]);
+                result.should.not.containEql(expected_elements[1]);
+                done();
+            });
+        });
+    });
 });
