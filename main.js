81932ee33dedee6cf330e03c6f5b12646821e40e
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2014-12-11 20:50:47 -0200

    now taking screenshots correctly

---
+++ main.js
@@ -0,0 +1,23 @@
+var App = require("./lib/App"),
+    ScreenSaverApp = require("./lib/ScreenSaverApp"),
+    webdriver = require("selenium-webdriver"),
+    fs = require("fs"),
+    app, driver, url;
+
+if (process.argv.length < 3)
+    process.exit(1);
+
+url = process.argv[2];
+
+driver = new webdriver.Builder()
+                      .withCapabilities(webdriver.Capabilities.firefox())
+                      .build();
+
+driver.get(url).then(function () {
+    app = ScreenSaverApp(App(driver, webdriver), driver, fs, "captured_widgets");
+    console.log("Looking for widgets...");
+    app.find_all_widgets().then(function (widgets) {
+        console.log("- Found: " + widgets.length);
+        process.exit(0);
+    });
+});
