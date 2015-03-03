38fd00de830ed132db7b988b05d98ea53baf5962
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-03 17:23:39 -0300

    now not logging mutations in role=log elements

--- tests/functional/App.js
+++ tests/functional/App.js
@@ -166,6 +166,28 @@ describe("App", function () {
                   });
         });
 
+        it("should not return mutations on log elements", function (done) {
+            var driver,
+                app;
+            driver = new webdriver.Builder()
+                                  .withCapabilities(webdriver.Capabilities.firefox())
+                                  .build();
+            driver.get(["file://", process.env["PWD"], "/tests/fixture/multiple_mutations03.html"].join(""))
+                  .then(function () {
+                      var hover_target, promises = [];
+                      app = App(driver, webdriver);
+                      app.find_widget(driver.findElement({css: "#link2"})).then(function (widget) {
+                          widget.should.have.lengthOf(1);
+                          return widget[0].getOuterHtml();
+                      }).then(function (html) {
+                          html.should.containDeep("Useful 3").and
+                                     .containDeep("<div");
+                          driver.quit();
+                          done();
+                      });
+                  });
+        });
+
     });
 
     describe("#find_all_widgets", function () {
