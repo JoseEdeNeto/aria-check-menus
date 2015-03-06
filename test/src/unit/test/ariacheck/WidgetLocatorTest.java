0a4050f2787242a6f0e282280e88c0d979799403
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-06 15:30:47 -0300

    WIP: now needs to mouse over elements and check their visibility again

--- test/src/unit/test/ariacheck/WidgetLocatorTest.java
+++ test/src/unit/test/ariacheck/WidgetLocatorTest.java
@@ -60,4 +60,30 @@ public class WidgetLocatorTest {
         assertEquals(childs_list.get(5), result);
     }
 
+    @Test
+    public void test_widget_locator_should_return_only_initially_invisible_elements_with_became_visible () {
+        WebDriver driver_mock = mock(WebDriver.class);
+        WebElement target_mock = mock(WebElement.class),
+                   result;
+        WidgetLocator locator = new WidgetLocator(driver_mock);
+        List <WebElement> childs_list = new ArrayList <WebElement> ();
+        childs_list.add(mock(WebElement.class));
+        childs_list.add(mock(WebElement.class));
+        childs_list.add(mock(WebElement.class));
+        childs_list.add(mock(WebElement.class));
+        childs_list.add(mock(WebElement.class));
+        childs_list.add(mock(WebElement.class));
+
+        when(childs_list.get(0).isDisplayed()).thenReturn(false).thenReturn(false);
+        when(childs_list.get(1).isDisplayed()).thenReturn(false).thenReturn(false);
+        when(childs_list.get(2).isDisplayed()).thenReturn(false).thenReturn(true);
+        when(childs_list.get(3).isDisplayed()).thenReturn(false).thenReturn(false);
+        when(childs_list.get(4).isDisplayed()).thenReturn(false).thenReturn(false);
+        when(childs_list.get(5).isDisplayed()).thenReturn(false).thenReturn(false);
+
+        when(driver_mock.findElements(By.cssSelector("body *"))).thenReturn(childs_list);
+
+        result = locator.find_widget(target_mock);
+        assertEquals(childs_list.get(2), result);
+    }
 }
