7fdf2bb3278a43b09b0077bb83ca50143489baab
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-06 17:41:23 -0300

    WIP: now mouse hovering, but it still need a few things implemented.

--- src/ariacheck/WidgetLocator.java
+++ src/ariacheck/WidgetLocator.java
@@ -3,23 +3,40 @@ package ariacheck;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
+import java.util.ArrayList;
 
 public class WidgetLocator {
 
     private WebDriver driver;
+    private Actions actions;
 
-    public WidgetLocator (WebDriver driver) {
+    public WidgetLocator (WebDriver driver, Actions actions) {
         this.driver = driver;
+        this.actions = actions;
     }
 
     public WebElement find_widget (WebElement target) {
         List <WebElement> child_elements = this.driver.findElements(By.cssSelector("body *"));
+        List <WebElement> invisibles = new ArrayList <WebElement> ();
+
         for (WebElement child : child_elements) {
             if ( ! child.isDisplayed())
-                return child;
+                invisibles.add(child);
         }
+
+        this.actions.moveByOffset(-1500, -1500)
+                    .moveToElement(target)
+                    .build()
+                    .perform();
+
+        for (WebElement inv : invisibles) {
+            if (inv.isDisplayed())
+                return inv;
+        }
+
         return child_elements.get(0);
     }
 
--- test/src/unit/test/ariacheck/WidgetLocatorTest.java
+++ test/src/unit/test/ariacheck/WidgetLocatorTest.java
@@ -4,6 +4,7 @@ import ariacheck.WidgetLocator;
 
 import static org.mockito.Mockito.*;
 import static org.junit.Assert.assertEquals;
+import org.mockito.InOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,6 +12,8 @@ import org.junit.runners.JUnit4;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.Action;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -21,9 +24,10 @@ public class WidgetLocatorTest {
     @Test
     public void test_widget_locator_should_call_findElements_in_driver_and_return_result () {
         WebDriver driver_mock = mock(WebDriver.class);
+        Actions actions_mock = mock(Actions.class);
         WebElement target_mock = mock(WebElement.class),
                    result;
-        WidgetLocator locator = new WidgetLocator(driver_mock);
+        WidgetLocator locator = new WidgetLocator(driver_mock, actions_mock);
         List <WebElement> childs_list = new ArrayList <WebElement> ();
         childs_list.add(mock(WebElement.class));
 
@@ -36,9 +40,10 @@ public class WidgetLocatorTest {
     @Test
     public void test_widget_locator_should_return_only_initially_invisible_elements () {
         WebDriver driver_mock = mock(WebDriver.class);
+        Actions actions_mock = mock(Actions.class);
         WebElement target_mock = mock(WebElement.class),
                    result;
-        WidgetLocator locator = new WidgetLocator(driver_mock);
+        WidgetLocator locator = new WidgetLocator(driver_mock, actions_mock);
         List <WebElement> childs_list = new ArrayList <WebElement> ();
         childs_list.add(mock(WebElement.class));
         childs_list.add(mock(WebElement.class));
@@ -63,9 +68,12 @@ public class WidgetLocatorTest {
     @Test
     public void test_widget_locator_should_return_only_initially_invisible_elements_with_became_visible () {
         WebDriver driver_mock = mock(WebDriver.class);
+        Actions actions_mock = mock(Actions.class);
+        Action action_mock = mock(Action.class);
         WebElement target_mock = mock(WebElement.class),
                    result;
-        WidgetLocator locator = new WidgetLocator(driver_mock);
+        InOrder inorder = inOrder(actions_mock, action_mock);
+        WidgetLocator locator = new WidgetLocator(driver_mock, actions_mock);
         List <WebElement> childs_list = new ArrayList <WebElement> ();
         childs_list.add(mock(WebElement.class));
         childs_list.add(mock(WebElement.class));
@@ -82,8 +90,17 @@ public class WidgetLocatorTest {
         when(childs_list.get(5).isDisplayed()).thenReturn(false).thenReturn(false);
 
         when(driver_mock.findElements(By.cssSelector("body *"))).thenReturn(childs_list);
+        when(actions_mock.moveByOffset(-1500, -1500)).thenReturn(actions_mock);
+        when(actions_mock.moveToElement(target_mock)).thenReturn(actions_mock);
+        when(actions_mock.build()).thenReturn(action_mock);
 
         result = locator.find_widget(target_mock);
+
+        inorder.verify(actions_mock).moveByOffset(-1500, -1500);
+        inorder.verify(actions_mock).moveToElement(target_mock);
+        inorder.verify(actions_mock).build();
+        inorder.verify(action_mock).perform();
+
         assertEquals(childs_list.get(2), result);
     }
 }
