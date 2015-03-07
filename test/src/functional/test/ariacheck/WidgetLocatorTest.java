acb05c4e53efd5cb3d90acfaa1405eeea98b99fd
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-07 00:58:45 -0300

    first functional test running...

--- test/src/functional/test/ariacheck/WidgetLocatorTest.java
+++ test/src/functional/test/ariacheck/WidgetLocatorTest.java
@@ -8,12 +8,32 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.interactions.Actions;
+
+import java.io.IOException;
+import java.io.File;
+
 @RunWith(JUnit4.class)
 public class WidgetLocatorTest {
 
     @Test
-    public void test_widget_locator_should_find_css_only_tooltip () {
-        WidgetLocator locator = new WidgetLocator();
+    public void test_widget_locator_should_find_css_only_tooltip () throws IOException {
+        WebDriver driver = new FirefoxDriver();
+        Actions actions = new Actions(driver);
+        WebElement target, result_widget;
+
+        driver.get("file://" + (new File(".").getCanonicalPath()) + "/test/fixture/sanity_check01.html");
+        target = driver.findElement(By.cssSelector("#link1"));
+
+        WidgetLocator locator = new WidgetLocator(driver, actions);
+        result_widget = locator.find_widget(target);
+
+        assertEquals("Useful message 1", result_widget.getText());
+        driver.quit();
     }
 
-}
+    }
