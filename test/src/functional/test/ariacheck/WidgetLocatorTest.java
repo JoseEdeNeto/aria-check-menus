d93c7a8e4ad46bb6acc6009006b0013db5c4664e
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-07 23:49:45 -0300

    functional test for the next task: recognizing mutations

--- test/src/functional/test/ariacheck/WidgetLocatorTest.java
+++ test/src/functional/test/ariacheck/WidgetLocatorTest.java
@@ -41,4 +41,21 @@ public class WidgetLocatorTest {
         driver.quit();
     }
 
+    @Test
+    public void test_widget_locator_should_find_widget_dynamically_inserted_in_the_DOM () throws IOException {
+        WebDriver driver = new FirefoxDriver();
+        Actions actions = new Actions(driver);
+        WebElement target, result_widget;
+
+        driver.get("file://" + (new File(".").getCanonicalPath()) + "/test/fixture/sanity_check02.html");
+        target = driver.findElement(By.cssSelector("#link3"));
+
+        WidgetLocator locator = new WidgetLocator(driver, actions);
+        result_widget = locator.find_widget(target);
+
+        assertEquals("Useful 3", result_widget.getText());
+
+        driver.quit();
+    }
+
 }
