b9f706c9bc261be51f49864ec8eeee061e78dd3d
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-09 18:54:36 -0300

    now canceling carousels via JavaScript interventions

--- test/src/functional/test/ariacheck/WidgetLocatorTest.java
+++ test/src/functional/test/ariacheck/WidgetLocatorTest.java
@@ -119,4 +119,38 @@ public class WidgetLocatorTest {
 
         driver.quit();
     }
+
+    @Test
+    public void test_widget_locator_should_ignore_carousels () throws IOException {
+        FirefoxDriver driver = new FirefoxDriver();
+        Actions actions = new Actions((WebDriver) driver);
+        WebElement result_widget;
+
+        driver.get("file://" + (new File(".").getCanonicalPath()) + "/test/fixture/carousel_01.html");
+
+        WidgetLocator locator = new WidgetLocator((WebDriver) driver, (JavascriptExecutor) driver, actions);
+
+        result_widget = locator.find_widget(driver.findElement(By.cssSelector("#link2")));
+        assertEquals("Useful 3", result_widget.getText());
+        assertEquals("div", result_widget.getTagName());
+
+        driver.quit();
+    }
+
+    @Test
+    public void test_widget_locator_should_ignore_carousels_which_restart () throws IOException {
+        FirefoxDriver driver = new FirefoxDriver();
+        Actions actions = new Actions((WebDriver) driver);
+        WebElement result_widget;
+
+        driver.get("file://" + (new File(".").getCanonicalPath()) + "/test/fixture/carousel_02.html");
+
+        WidgetLocator locator = new WidgetLocator((WebDriver) driver, (JavascriptExecutor) driver, actions);
+
+        result_widget = locator.find_widget(driver.findElement(By.cssSelector("#link2")));
+        assertEquals("Useful 3", result_widget.getText());
+        assertEquals("div", result_widget.getTagName());
+
+        driver.quit();
+    }
 }
