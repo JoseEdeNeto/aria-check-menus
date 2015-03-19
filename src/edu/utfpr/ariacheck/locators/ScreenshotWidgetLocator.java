cf6aad5d0b82cb25910824f8b5c45753fc862754
Willian Massami Watanabe <watanabe_willian@yahoo.com>
2015-03-19 20:42:48 -0300

    WIP: now taking one screenshot, but it still need another one after the find_widget command.

--- src/edu/utfpr/ariacheck/locators/ScreenshotWidgetLocator.java
+++ src/edu/utfpr/ariacheck/locators/ScreenshotWidgetLocator.java
@@ -1,14 +1,47 @@
 package edu.utfpr.ariacheck.locators;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class ScreenshotWidgetLocator implements Locator {
 
+    private Locator decorable;
+    private TakesScreenshot takes;
+    private String folder;
+
+    public ScreenshotWidgetLocator (Locator locator, TakesScreenshot takes, String folder) {
+        this.decorable = locator;
+        this.takes = takes;
+        this.folder = folder;
+    }
+
     public WebElement find_widget (WebElement target) {
-        return null;
+        String new_file_path = this.folder + "001_before_widget.jpg";
+        File screenshot = this.takes.getScreenshotAs(OutputType.FILE),
+             new_file = this.create_file_wrapper(new_file_path);
+        try {
+            this.copy_file_wrapper(screenshot, new_file);
+        } catch (IOException io) {
+            System.out.println("Error copying screenshot file to path: " + new_file_path);
+        }
+        return this.decorable.find_widget(target);
+    }
+
+    public File create_file_wrapper (String name) {
+        return new File(name);
+    }
+
+    public void copy_file_wrapper (File screenshot, File new_file) throws IOException {
+        FileUtils.copyFile(screenshot, new_file);
     }
 
 }
--- test/src/unit/test/edu/utfpr/ariacheck/locators/ScreenshotWidgetLocatorTest.java
+++ test/src/unit/test/edu/utfpr/ariacheck/locators/ScreenshotWidgetLocatorTest.java
@@ -1,5 +1,6 @@
 package unit.test.edu.utfpr.ariacheck.locators;
 
+import edu.utfpr.ariacheck.locators.Locator;
 import edu.utfpr.ariacheck.locators.WidgetLocator;
 import edu.utfpr.ariacheck.locators.ScreenshotWidgetLocator;
 
@@ -12,14 +13,37 @@ import org.junit.runners.JUnit4;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.interactions.Actions;
+
+import java.io.File;
+import java.io.IOException;
 
 @RunWith(JUnit4.class)
 public class ScreenshotWidgetLocatorTest {
 
     @Test
-    public void test_it_should_be_an_instance_of_widget_locator () {
+    public void test_find_widget_should_call_find_widget_in_another_Locator_and_take_two_screenshots () throws IOException {
+        Locator locator_mock = mock(Locator.class);
+        TakesScreenshot takes_mock = mock(TakesScreenshot.class);
+        File screenshot_stub = mock(File.class),
+             new_file_stub = mock(File.class);
+        WebElement target_stub = mock(WebElement.class),
+                   widget_stub = mock(WebElement.class),
+                   result = null;
+        when(locator_mock.find_widget(target_stub)).thenReturn(widget_stub);
+        when(takes_mock.getScreenshotAs(OutputType.FILE)).thenReturn(screenshot_stub);
+
+        ScreenshotWidgetLocator locator = new ScreenshotWidgetLocator(locator_mock, takes_mock, "captured_screens/");
+        locator = spy(locator);
+        when(locator.create_file_wrapper("captured_screens/001_before_widget.jpg")).thenReturn(new_file_stub);
+        doNothing().when(locator).copy_file_wrapper(screenshot_stub, new_file_stub);
+        result = locator.find_widget(target_stub);
+
+        verify(locator).copy_file_wrapper(screenshot_stub, new_file_stub);
+        assertEquals(widget_stub, result);
     }
 
 }
