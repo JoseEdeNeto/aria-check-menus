b07c24610ef523837a6b7e9f8fda2d28cabcca09
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-06-09 21:18:21 -0300

    now also taking screenshots of activator and widgets

--- src/main/java/edu/utfpr/ariacheck/locators/decorators/ScreenshotWidgetLocatorDecorator.java
+++ src/main/java/edu/utfpr/ariacheck/locators/decorators/ScreenshotWidgetLocatorDecorator.java
@@ -4,6 +4,7 @@ import edu.utfpr.ariacheck.locators.Locator;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.OutputType;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -11,8 +12,10 @@ import org.openqa.selenium.interactions.Actions;
 
 import org.apache.commons.io.FileUtils;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class ScreenshotWidgetLocatorDecorator implements Locator {
 
@@ -21,6 +24,8 @@ public class ScreenshotWidgetLocatorDecorator implements Locator {
     private String folder;
     private int counter = 1;
     private String image_filetype = ".jpg";
+
+    public void setCounter (int counter) { this.counter = counter; }
 
     public ScreenshotWidgetLocatorDecorator (Locator locator, TakesScreenshot takes, String folder) {
         this.decorable = locator;
@@ -43,13 +48,16 @@ public class ScreenshotWidgetLocatorDecorator implements Locator {
             System.out.println("Error copying screenshot file to path: before widget");
         }
         screenshot = this.takes.getScreenshotAs(OutputType.FILE);
-        new_file = this.create_file_wrapper(this.folder + (String.format("%03d", this.counter++)) +
+        new_file = this.create_file_wrapper(this.folder + (String.format("%03d", this.counter)) +
                                             "_later_widget" + this.image_filetype);
         try {
             this.copy_file_wrapper(screenshot, new_file);
         } catch (IOException io) {
             System.out.println("Error copying screenshot file to path: later widget");
         }
+        this.save_element_screenshot(target, screenshot, "widget_activator");
+        this.save_element_screenshot(widget, screenshot, "widget");
+        this.counter++;
         return widget;
     }
 
@@ -63,6 +71,39 @@ public class ScreenshotWidgetLocatorDecorator implements Locator {
 
     public void set_image_filetype (String filetype) {
         this.image_filetype = filetype;
+    }
+
+    public void save_element_screenshot (WebElement target, File screenshot, String filename) {
+        BufferedImage full_image = this.imageio_read_wrapper(screenshot),
+                      sub_image = null;
+        int left = target.getLocation().getX(),
+            top = target.getLocation().getY(),
+            height = target.getSize().getHeight(),
+            width = target.getSize().getWidth();
+        sub_image = full_image.getSubimage(
+                left, top, left + width, top + height);
+        File file = this.create_file_wrapper(this.folder + (String.format("%03d", this.counter++)) +
+                                            "_" + filename + this.image_filetype);
+        this.imageio_write_wrapper(sub_image, file);
+    }
+
+    public BufferedImage imageio_read_wrapper (File screenshot) {
+        try {
+            return ImageIO.read(screenshot);
+        } catch (IOException exception) {
+            System.out.println("Could not convert screenshot to BufferedImage...");
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    public void imageio_write_wrapper (BufferedImage buf_image, File file) {
+        try {
+            ImageIO.write(buf_image, "jpg", file);
+        } catch (IOException ex) {
+            System.out.println("Could not save buffered image...");
+            ex.printStackTrace();
+        }
     }
 
 }
--- src/test/java/unit/test/edu/utfpr/ariacheck/locators/ScreenshotWidgetLocatorTest.java
+++ src/test/java/unit/test/edu/utfpr/ariacheck/locators/ScreenshotWidgetLocatorTest.java
@@ -12,15 +12,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.interactions.Actions;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import javax.imageio.ImageIO;
 
 @RunWith(JUnit4.class)
 public class ScreenshotWidgetLocatorTest {
@@ -47,6 +51,9 @@ public class ScreenshotWidgetLocatorTest {
         when(locator.create_file_wrapper("captured_screens/001_later_widget.jpg")).thenReturn(new_file_stub_2);
         doNothing().when(locator).copy_file_wrapper(screenshot_stub, new_file_stub);
         doNothing().when(locator).copy_file_wrapper(screenshot_stub_2, new_file_stub_2);
+        doNothing().when(locator).save_element_screenshot(target_stub, screenshot_stub_2, "widget_activator");
+        doNothing().when(locator).save_element_screenshot(widget_stub, screenshot_stub_2, "widget");
+
         result = locator.find_widget(target_stub);
 
         inorder.verify(takes_mock).getScreenshotAs(OutputType.FILE);
@@ -56,6 +63,8 @@ public class ScreenshotWidgetLocatorTest {
         inorder.verify(takes_mock).getScreenshotAs(OutputType.FILE);
         inorder.verify(locator).create_file_wrapper("captured_screens/001_later_widget.jpg");
         inorder.verify(locator).copy_file_wrapper(screenshot_stub_2, new_file_stub_2);
+        inorder.verify(locator).save_element_screenshot(target_stub, screenshot_stub_2, "widget_activator");
+        inorder.verify(locator).save_element_screenshot(widget_stub, screenshot_stub_2, "widget");
         assertEquals(widget_stub, result);
     }
 
@@ -77,6 +86,8 @@ public class ScreenshotWidgetLocatorTest {
         inorder = inOrder(locator, locator_mock);
         when(locator.create_file_wrapper(anyString())).thenReturn(new_file_stub);
         doNothing().when(locator).copy_file_wrapper(screenshot_stub, new_file_stub);
+        doNothing().when(locator).save_element_screenshot(target_stub, screenshot_stub, "widget_activator");
+        doNothing().when(locator).save_element_screenshot(widget_stub, screenshot_stub, "widget");
         result = locator.find_widget(target_stub);
         result = locator.find_widget(target_stub);
         result = locator.find_widget(target_stub);
@@ -133,5 +144,39 @@ public class ScreenshotWidgetLocatorTest {
         verify(locator, never()).create_file_wrapper("captured_screens/001_later_widget.jpg");
         verify(locator, never()).copy_file_wrapper(screenshot_stub_2, new_file_stub_2);
         assertEquals(null, result);
+    }
+
+    @Test
+    public void test_save_element_screenshot () {
+        Locator locator_mock = mock(Locator.class);
+        TakesScreenshot takes_mock = mock(TakesScreenshot.class);
+        ScreenshotWidgetLocatorDecorator locator = spy(new ScreenshotWidgetLocatorDecorator(
+                    locator_mock, takes_mock, "captured_screens/"));
+        WebElement target_stub = mock(WebElement.class);
+        Point target_location_stub = mock(Point.class);
+        Dimension target_dimension_stub = mock(Dimension.class);
+        File screenshot_stub = mock(File.class);
+        BufferedImage buf_mock = mock(BufferedImage.class),
+                      subimage_buf_stub = mock(BufferedImage.class);
+        File file_stub = mock(File.class);
+
+        doReturn(buf_mock).when(locator).imageio_read_wrapper(screenshot_stub);
+        doReturn(target_location_stub).when(target_stub).getLocation();
+        doReturn(15).when(target_location_stub).getX();
+        doReturn(75).when(target_location_stub).getY();
+        doReturn(target_dimension_stub).when(target_stub).getSize();
+        doReturn(77).when(target_dimension_stub).getWidth();
+        doReturn(98).when(target_dimension_stub).getHeight();
+        doReturn(subimage_buf_stub).when(buf_mock).getSubimage(
+                15, 75, 92, 173);
+        doReturn(file_stub).when(locator).create_file_wrapper("captured_screens/013_widget_activator.jpg");
+        locator.setCounter(13);
+        doNothing().when(locator).imageio_write_wrapper(subimage_buf_stub, file_stub);
+
+        locator.save_element_screenshot(target_stub, screenshot_stub, "widget_activator");
+
+        verify(locator).imageio_read_wrapper(screenshot_stub);
+        verify(buf_mock).getSubimage(15, 75, 92, 173);
+        verify(locator).imageio_write_wrapper(subimage_buf_stub, file_stub);
     }
 }
