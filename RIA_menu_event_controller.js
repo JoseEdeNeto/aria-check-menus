831c9d98ba06cd6510a8e5ea8673b22c58d46f72
Willian Massami Watanabe <wwatanabe@utfpr.edu.br>
2016-07-04 21:45:43 -0300

    waiting a little longer to make sure the script is run smoothly

--- RIA_menu_event_controller.js
+++ RIA_menu_event_controller.js
@@ -33,44 +33,46 @@ page.onInitialized = function () {
 page.settings.XSSAuditingEnabled = true;
 page.settings.webSecurityEnabled = false;
 page.open(system.args[1], function () {
-    console.log(page.evaluate(function () {
-        // onmouseover listeners
-        var all = document.querySelectorAll("*");
-        for (var l = 0; l < all.length; l++) {
-            if (all[l].onmouseover) {
-                var target = all[l],
-                    selector = "";
+    setTimeout(function () {
+        console.log(page.evaluate(function () {
+            // onmouseover listeners
+            var all = document.querySelectorAll("*");
+            for (var l = 0; l < all.length; l++) {
+                if (all[l].onmouseover) {
+                    var target = all[l],
+                        selector = "";
 
-                while (target.parentElement != null) {
-                    selector = (selector.length === 0 ?
-                        target.tagName.toLowerCase() :
-                        target.tagName.toLowerCase() + " > " + selector);
-                    target = target.parentElement;
+                    while (target.parentElement != null) {
+                        selector = (selector.length === 0 ?
+                            target.tagName.toLowerCase() :
+                            target.tagName.toLowerCase() + " > " + selector);
+                        target = target.parentElement;
+                    }
+                    if (selector.length > 0)
+                        window.events.push(selector);
                 }
-                if (selector.length > 0)
-                    window.events.push(selector);
-            }
-        };
+            };
 
-        // CSS :hover selectors
-        for (var i = 0; i < document.styleSheets.length; i++) {
-            for (var j = 0; document.styleSheets[i].cssRules &&
-                            j < document.styleSheets[i].cssRules.length; j++) {
-                if (document.styleSheets[i].cssRules[j].selectorText) {
-                    var rules = document.styleSheets[i].cssRules[j].selectorText.split(",");
-                    for (var k = 0; k < rules.length; k++) {
-                        if (rules[k].search(":hover") > 0) {
-                            var s = rules[k].substring(0, rules[k].search(":hover")).trim();
-                            try {
-                                document.querySelector(s);
-                            } catch (e) { s += " *"; }
-                            window.events.push(s);
-                        }
-                    };
+            // CSS :hover selectors
+            for (var i = 0; i < document.styleSheets.length; i++) {
+                for (var j = 0; document.styleSheets[i].cssRules &&
+                                j < document.styleSheets[i].cssRules.length; j++) {
+                    if (document.styleSheets[i].cssRules[j].selectorText) {
+                        var rules = document.styleSheets[i].cssRules[j].selectorText.split(",");
+                        for (var k = 0; k < rules.length; k++) {
+                            if (rules[k].search(":hover") > 0) {
+                                var s = rules[k].substring(0, rules[k].search(":hover")).trim();
+                                try {
+                                    document.querySelector(s);
+                                } catch (e) { s += " *"; }
+                                window.events.push(s);
+                            }
+                        };
+                    }
                 }
             }
-        }
-        return window.events.join(",");
-    }));
-    phantom.exit();
+            return window.events.join(",");
+        }));
+        phantom.exit();
+    }, 10000);
 });
